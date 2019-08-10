a9e3b069ed63758fc53476351e211f68707b22b7
umeraliripon <you@example.com>
2019-08-10 22:47:38 +0600

    f

--- helper.php
+++ helper.php
@@ -9,10 +9,11 @@ require dirname(__FILE__).'/elements/section-title.php';
 require dirname(__FILE__).'/elements/service-section.php';
 require dirname(__FILE__).'/elements/counter.php';
 require dirname(__FILE__).'/elements/progressbar.php';
-
-
 require dirname(__FILE__).'/elements/team1.php';
-
+require dirname(__FILE__).'/elements/team2.php';
+require dirname(__FILE__).'/elements/team3.php';
 require dirname(__FILE__).'/elements/clints.php';
+
+require dirname(__FILE__).'/elements/h-slider.php';
 ?>
 ?>
